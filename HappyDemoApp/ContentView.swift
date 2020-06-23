1ac63c072abe6eb3664e09f437c691dd92183b00
Farshad Mousalou <farshadm90@gmail.com>
2020-06-23 14:42:28 +0430

    - create xcode project files.

---
+++ HappyDemoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HappyDemoApp
+//
+//  Created by Farshad Mousalou on 6/23/20.
+//  Copyright © 2020 Farshad Mousalou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
