559c49950105d4c9de2bfa82af4a7d196046bfe9
Farshad Mousalou <farshadm90@gmail.com>
2020-06-23 17:51:10 +0430

    - Added and Declare Data Layer Abstract and protocols.

---
+++ HappyDemoApp/AppStructure/infrastructure - Data/Interfaces/Repository/DatabaseRepository.swift
@@ -0,0 +1,15 @@
+//
+//  DatabaseRepository.swift
+//  HappyDemoApp
+//
+//  Created by Farshad Mousalou on 6/23/20.
+//  Copyright © 2020 Farshad Mousalou. All rights reserved.
+//
+
+import Foundation
+
+protocol DatabaseRepository {
+    
+    init(storage: Storage)
+    
+}
