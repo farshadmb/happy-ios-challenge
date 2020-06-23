abe07957ad837fe26cd1bd5ba071905a24e3e778
Farshad Mousalou <farshadm90@gmail.com>
2020-06-23 18:00:40 +0430

    - added and declared domain layer abstracts and interfaces.

---
+++ HappyDemoApp/AppStructure/Domain/Interfaces/Entity/Entity.swift
@@ -0,0 +1,13 @@
+//
+//  Entity.swift
+//  HappyDemoApp
+//
+//  Created by Farshad Mousalou on 6/23/20.
+//  Copyright © 2020 Farshad Mousalou. All rights reserved.
+//
+
+import Foundation
+
+protocol Entity {
+    
+}
